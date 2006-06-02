b18c2fe55f12382d8001c87abb4950e1ff90a63b
Peter Thomas <ptrthomas@gmail.com>
2006-06-02 11:59:13 +0000

    improvement: default beanshell file to run can be configured in pom.xml

--- maven-bshrunner-plugin/src/main/java/info/jtrac/maven/BshRunnerMojo.java
+++ maven-bshrunner-plugin/src/main/java/info/jtrac/maven/BshRunnerMojo.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,47 +37,58 @@ import bsh.Interpreter;
 public class BshRunnerMojo extends AbstractMojo {
 
     /**
+     * @parameter
+     */
+    private String file;
+
+    /**
      * @parameter expression="${project}"
      */
     private MavenProject project;
-    
+
     /**
      * @parameter expression="${localRepository}"
      */
     private ArtifactRepository localRepository;
-    
+
     /**
      * @component role="org.apache.maven.artifact.resolver.ArtifactResolver"
      */
-    private ArtifactResolver artifactResolver;    
-    
+    private ArtifactResolver artifactResolver;
+
     /**
      * @parameter expression="${project.remoteArtifactRepositories}"
      */
     private List remoteArtifactRepositories;
-    
+
     /**
      * @component role="org.apache.maven.artifact.factory.ArtifactFactory"
      */
-    private ArtifactFactory artifactFactory;    
-    
+    private ArtifactFactory artifactFactory;
+
     /**
      * @component role="org.apache.maven.project.MavenProjectBuilder"
      */
-    private MavenProjectBuilder mavenProjectBuilder;  
-    
+    private MavenProjectBuilder mavenProjectBuilder;
+
 	public void execute() throws MojoExecutionException {
-		
-		String bshFile = System.getProperty("bsh.file");
-		if (bshFile == null) {
-			throw new MojoExecutionException("bsh.file system property not set");
+
+		if (file == null) {
+			file = System.getProperty("bsh.file");
 		}
-		File file = new File(bshFile);
-		if (!file.exists()) {
-			throw new MojoExecutionException("file does not exist: " + bshFile);
+
+		if (file == null) {
+			throw new MojoExecutionException("no file specified in plugin config and bsh.file system property not set");
 		}
-		
+
+		File f = new File(file);
+
+		if (!f.exists()) {
+			throw new MojoExecutionException("file does not exist: " + f);
+		}
+
 		Interpreter i = new Interpreter();
+
 		try {
 			i.set("project", project);
 			i.set("localRepository", localRepository);
@@ -85,14 +96,14 @@ public class BshRunnerMojo extends AbstractMojo {
 			i.set("remoteArtifactRepositories", remoteArtifactRepositories);
 			i.set("artifactFactory", artifactFactory);
 			i.set("mavenProjectBuilder", mavenProjectBuilder);
-			getLog().info("executing beanshell script file: " + bshFile);
-			i.source(bshFile);
+			getLog().info("executing beanshell script file: " + f);
+			i.source(file);
 			getLog().info("beanshell script executed successfully");
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new MojoExecutionException(e.getLocalizedMessage());
 		}
-		
+
 	}
-	
+
 }
