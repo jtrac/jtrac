637a42bee36ef65de33b97f52b044520d624e2fa
Peter Thomas <ptrthomas@gmail.com>
2006-08-07 14:40:31 +0000

    first pass at refactoring the maven beanshell runner plugin

--- maven-bshrunner-plugin/src/main/java/info/jtrac/maven/BshRunnerMojo.java
+++ maven-bshrunner-plugin/src/main/java/info/jtrac/maven/BshRunnerMojo.java
@@ -17,11 +17,16 @@
 package info.jtrac.maven;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
@@ -71,6 +76,8 @@ public class BshRunnerMojo extends AbstractMojo {
      */
     private MavenProjectBuilder mavenProjectBuilder;
 
+    //  ========================== MAIN ========================
+    
 	public void execute() throws MojoExecutionException {
 
 		String temp = System.getProperty("bsh.file");
@@ -92,12 +99,8 @@ public class BshRunnerMojo extends AbstractMojo {
 		Interpreter i = new Interpreter();
 
 		try {
+			i.set("mvn", this);
 			i.set("project", project);
-			i.set("localRepository", localRepository);
-			i.set("artifactResolver", artifactResolver);
-			i.set("remoteArtifactRepositories", remoteArtifactRepositories);
-			i.set("artifactFactory", artifactFactory);
-			i.set("mavenProjectBuilder", mavenProjectBuilder);
 			getLog().info("executing beanshell script file: " + f);
 			i.source(file);
 			getLog().info("beanshell script executed successfully");
@@ -107,5 +110,97 @@ public class BshRunnerMojo extends AbstractMojo {
 		}
 
 	}
+	
+	//========================== HELPER METHODS ======================
+	
+	/**
+	 * get the repository base folder converting to a java friendly path
+	 */
+	public String getBaseDir() {
+		return localRepository.getBasedir().replace('\\','/');
+	}	
+	
+	/**
+	 * instantiate a single artifact using Maven, on the fly
+	 */
+	public Artifact getArtifact(String groupId, String artifactId, String version) {
+	    return artifactFactory.createArtifact(groupId, artifactId, version, "", "jar");      
+	}
 
+	/**
+	 * resolve dependencies for the given artifact details using Maven, on the fly
+	 */
+	public Collection resolveDependencies(String groupId, String artifactId, String version) throws Exception {
+	    Artifact pomArtifact = getArtifact(groupId, artifactId, version); 
+	    MavenProject pomProject = mavenProjectBuilder.buildFromRepository(pomArtifact, remoteArtifactRepositories, localRepository);    
+	    Collection artifacts = pomProject.createArtifacts(artifactFactory, Artifact.SCOPE_TEST, new ScopeArtifactFilter(Artifact.SCOPE_TEST));
+	    Iterator i = artifacts.iterator();
+	    while(i.hasNext()) {
+	    	Artifact a = (Artifact) i.next();
+	        artifactResolver.resolve(a, remoteArtifactRepositories, localRepository);
+	    }
+	    artifacts.add(pomArtifact);
+	    return artifacts;      
+	}
+
+	/**
+	 * resolve single artifact to file, and resolve fully from repository if required
+	 */
+	public File resolveArtifact(Artifact artifact) throws Exception {
+	    File f = artifact.getFile();
+	    if (f != null) return f;
+	    getLog().info("*** resolving artifact: " + artifact);
+	    artifactResolver.resolve(artifact, remoteArtifactRepositories, localRepository);
+	    return artifact.getFile();    
+	}	
+	
+	/**
+	 * convert a collection of maven artifacts into a collection of files
+	 */
+	public Collection getFiles(Collection artifacts) throws Exception {
+	    Collection files = new ArrayList();
+	    Iterator i = artifacts.iterator();
+	    while(i.hasNext()) {
+	    	Artifact a = (Artifact) i.next();
+	        files.add(resolveArtifact(a));
+	    }
+	    return files;
+	}
+	
+	/**
+	 * convenience combination of resolving and getting a bunch of files
+	 */
+	public Collection getFiles(String groupId, String artifactId, String version) throws Exception {
+	    return getFiles(resolveDependencies(groupId, artifactId, version));
+	}	
+	
+	/**
+	 * function to return relative path given base path and the target file
+	 */
+	public String getRelativePath(File file, String basePath) {
+	    String p = basePath.replace('\\','/');
+	    int len = p.length() + 1;
+	    if (p.endsWith("/")) len--;
+	    return file.getPath().substring(len).replace('\\','/');
+	}
+	
+	/**
+	 * generate path entries for the given bunch of files
+	 */
+	public String getPaths(Collection files, String basePath, String baseKey) {
+		StringBuffer sb = new StringBuffer();
+		Iterator i = files.iterator();
+		while(i.hasNext()) {
+			File f = (File) i.next();
+	        String p = getRelativePath(f, basePath);
+	        sb.append("\\\n    ");
+	        if (baseKey == null) {
+		    sb.append(p + ",");
+	        } else {
+		    sb.append(baseKey + "/" + p + ":");
+	        }        
+	    }
+	    return sb.toString();
+	}
+	
 }
