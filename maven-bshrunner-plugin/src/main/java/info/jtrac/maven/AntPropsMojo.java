54f96d95fa61395be037576b535677d9d97a052b
Peter Thomas <ptrthomas@gmail.com>
2006-08-12 15:26:13 +0000

    moved away from beanshell, now a focused maven2 plugin, major changes

--- maven-bshrunner-plugin/src/main/java/info/jtrac/maven/AntPropsMojo.java
+++ maven-bshrunner-plugin/src/main/java/info/jtrac/maven/AntPropsMojo.java
@@ -17,10 +17,22 @@
 package info.jtrac.maven;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
@@ -32,15 +44,14 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectBuilder;
 
-import bsh.Interpreter;
-
 /**
- * @goal bshrunner
+ * @goal antprops
  * @requiresDependencyResolution runtime
- * @description ant helper script generator
+ * @description ant build properties file generator
  */
-public class BshRunnerMojo extends AbstractMojo {
+public class AntPropsMojo extends AbstractMojo {
 
+	//======================== MOJO ===============================
     /**
      * @parameter
      */
@@ -75,62 +86,51 @@ public class BshRunnerMojo extends AbstractMojo {
      * @component role="org.apache.maven.project.MavenProjectBuilder"
      */
     private MavenProjectBuilder mavenProjectBuilder;
-
-    //  ========================== MAIN ========================
+    
+    //======================== PRIVATE ===============================
+    
+    private Properties props;
+	private Map buildProperties = new LinkedHashMap();
+	private Map buildClassPaths = new LinkedHashMap();
+	private Set runtimeFiles;  
+	private String repoBaseDir;
+    
+    //========================== MAIN ================================
     
 	public void execute() throws MojoExecutionException {
-
-		String temp = System.getProperty("bsh.file");
-
-		if (temp != null) {
-			file = temp;
-		}
-
-		if (file == null) {
-			throw new MojoExecutionException("no file specified in plugin config and bsh.file system property not set");
-		}
-
-		File f = new File(file);
-
-		if (!f.exists()) {
-			throw new MojoExecutionException("file does not exist: " + f);
-		}
-
-		Interpreter i = new Interpreter();
-
+		repoBaseDir = localRepository.getBasedir().replace('\\','/');
 		try {
-			i.set("mvn", this);
-			i.set("project", project);
-			getLog().info("executing beanshell script file: " + f);
-			i.source(file);
-			getLog().info("beanshell script executed successfully");
+			buildProperties.put("m2.repo", repoBaseDir);
+			//========================================================
+			Collection runtimeArtifacts = project.getArtifacts();
+			runtimeFiles = getRelativePaths(getFiles(runtimeArtifacts), repoBaseDir);
+			//========================================================
+			Set testArtifacts = project.getDependencyArtifacts();
+			testArtifacts.addAll(project.getTestArtifacts());
+			Collection testFiles = getFiles(testArtifacts);
+			Set testPaths = getRelativePaths(testFiles, repoBaseDir);
+			buildClassPaths.put("test.jars", testPaths);
+			//========================================================
+			writeAntPropsFile();
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new MojoExecutionException(e.getLocalizedMessage());
 		}
-
 	}
 	
 	//========================== HELPER METHODS ======================
 	
 	/**
-	 * get the repository base folder converting to a java friendly path
-	 */
-	public String getBaseDir() {
-		return localRepository.getBasedir().replace('\\','/');
-	}	
-	
-	/**
 	 * instantiate a single artifact using Maven, on the fly
 	 */
-	public Artifact getArtifact(String groupId, String artifactId, String version) {
+	private Artifact getArtifact(String groupId, String artifactId, String version) {
 	    return artifactFactory.createArtifact(groupId, artifactId, version, "", "jar");      
 	}
 
 	/**
 	 * resolve dependencies for the given artifact details using Maven, on the fly
 	 */
-	public Collection resolveDependencies(String groupId, String artifactId, String version) throws Exception {
+	private Collection resolveDependencies(String groupId, String artifactId, String version) throws Exception {
 	    Artifact pomArtifact = getArtifact(groupId, artifactId, version); 
 	    MavenProject pomProject = mavenProjectBuilder.buildFromRepository(pomArtifact, remoteArtifactRepositories, localRepository);    
 	    Collection artifacts = pomProject.createArtifacts(artifactFactory, Artifact.SCOPE_TEST, new ScopeArtifactFilter(Artifact.SCOPE_TEST));
@@ -146,7 +146,7 @@ public class BshRunnerMojo extends AbstractMojo {
 	/**
 	 * resolve single artifact to file, and resolve fully from repository if required
 	 */
-	public File resolveArtifact(Artifact artifact) throws Exception {
+	private File resolveArtifact(Artifact artifact) throws Exception {
 	    File f = artifact.getFile();
 	    if (f != null) {
 	    	return f;
@@ -159,7 +159,7 @@ public class BshRunnerMojo extends AbstractMojo {
 	/**
 	 * convert a collection of maven artifacts into a collection of files
 	 */
-	public Collection getFiles(Collection artifacts) throws Exception {
+	private Collection getFiles(Collection artifacts) throws Exception {
 	    Collection files = new ArrayList();
 	    Iterator i = artifacts.iterator();
 	    while(i.hasNext()) {
@@ -172,14 +172,14 @@ public class BshRunnerMojo extends AbstractMojo {
 	/**
 	 * convenience combination of resolving and getting a bunch of files
 	 */
-	public Collection getFiles(String groupId, String artifactId, String version) throws Exception {
+	private Collection getFiles(String groupId, String artifactId, String version) throws Exception {
 	    return getFiles(resolveDependencies(groupId, artifactId, version));
 	}	
 	
 	/**
 	 * function to return relative path given base path and the target file
 	 */
-	public String getRelativePath(File file, String basePath) {
+	private String getRelativePath(File file, String basePath) {
 	    String p = basePath.replace('\\','/');
 	    int len = p.length() + 1;
 	    if (p.endsWith("/")) {
@@ -189,22 +189,69 @@ public class BshRunnerMojo extends AbstractMojo {
 	}
 	
 	/**
-	 * generate path entries for the given bunch of files
+	 * add path entries for the given bunch of files
 	 */
-	public String getPaths(Collection files, String basePath, String baseKey) {
-		StringBuffer sb = new StringBuffer();
+	private Set getRelativePaths(Collection files, String basePath) {
+		Set paths = new TreeSet();
 		Iterator i = files.iterator();
 		while (i.hasNext()) {
 			File f = (File) i.next();
-			String p = getRelativePath(f, basePath);
-			sb.append("\\\n    ");
-			if (baseKey == null) {
-				sb.append(p + ",");
-			} else {
-				sb.append(baseKey + "/" + p + ":");
-			}
+			String path = getRelativePath(f, basePath);
+			paths.add(path);
 		}
-		return sb.toString();
+		return paths;
+	}	
+	
+	/**
+	 * load properties from given file
+	 */
+	private void loadPropertiesFromFile(String fileName) throws Exception {		
+		InputStream is = new FileInputStream(fileName);
+		props = new Properties();
+		props.load(is);		
+		is.close();
+	}
+	
+	/**
+	 * get property, file has to have been loaded before calling
+	 */
+	private String getProperty(String key) {
+		return props.getProperty(key);
+	}
+	
+	/**
+	 * generate properties file with dependency information, ready to use by ant
+	 */
+	private void writeAntPropsFile() throws Exception {
+		OutputStream os = new FileOutputStream("build-deps.properties");
+		Writer out = new PrintWriter(os);
+		Date date = new Date();
+		out.write("# *** generated by the AntProps Maven2 plugin: " + date + " ***\n\n");
+		//==========================================================		
+		for (Iterator i = buildProperties.entrySet().iterator(); i.hasNext(); ) {
+			Map.Entry entry = (Map.Entry) i.next();
+			out.write(entry.getKey() + "=" + entry.getValue() + "\n\n");
+		}
+		//===========================================================
+		out.write("runtime.jars=");	
+		for (Iterator i = runtimeFiles.iterator(); i.hasNext(); ) {			
+			String path = (String) i.next();
+			out.write("\\\n    " + path + ",");
+		}
+		out.write("\n\n");
+		//===========================================================
+		for (Iterator i = buildClassPaths.entrySet().iterator(); i.hasNext(); ) {
+			Map.Entry entry = (Map.Entry) i.next();
+			out.write(entry.getKey() + "=");
+			Set paths = (Set) entry.getValue();
+			for (Iterator j = paths.iterator(); j.hasNext(); ) {
+				String path = (String) j.next();
+				out.write("\\\n    ${m2.repo}/" + path + ":");
+			}
+			out.write("\n\n");
+		}
+		out.close();
+		os.close();
 	}
 	
 }
