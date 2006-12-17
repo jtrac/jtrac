986f7f8c3fc1023a025ad9ab5df1a5f26ca899df
Peter Thomas <ptrthomas@gmail.com>
2006-12-17 17:10:48 +0000

    mylar work in progress, trying to get plugin to load

--- jtrac-mylar/src/info/jtrac/mylar/ui/JtracTaskExternalizer.java
+++ jtrac-mylar/src/info/jtrac/mylar/ui/JtracTaskExternalizer.java
@@ -16,8 +16,53 @@
 
 package info.jtrac.mylar.ui;
 
+import info.jtrac.mylar.JtracRepositoryQuery;
+import info.jtrac.mylar.JtracRepositoryTask;
+
+import org.eclipse.mylar.tasks.core.AbstractRepositoryQuery;
 import org.eclipse.mylar.tasks.core.DelegatingTaskExternalizer;
+import org.eclipse.mylar.tasks.core.ITask;
+import org.w3c.dom.Node;
 
 public class JtracTaskExternalizer extends DelegatingTaskExternalizer {
 
+	private static final String KEY_JTRAC = "JTrac";
+	private static final String KEY_JTRAC_CATEGORY = KEY_JTRAC + KEY_CATEGORY;
+	private static final String KEY_JTRAC_TASK = KEY_JTRAC + KEY_TASK;
+	private static final String KEY_JTRAC_QUERY_HIT = KEY_JTRAC + KEY_QUERY_HIT;
+	private static final String KEY_JTRAC_QUERY = KEY_JTRAC + KEY_QUERY;	
+	
+	@Override
+	public boolean canReadCategory(Node node) {
+		return node.getNodeName().equals(KEY_JTRAC_CATEGORY);
+	}	
+	
+	@Override
+	public String getCategoryTagName() {
+		return KEY_JTRAC_CATEGORY;
+	}	
+	
+	@Override
+	public boolean canCreateElementFor(ITask task) {
+		return task instanceof JtracRepositoryTask;
+	}
+	
+	@Override
+	public String getTaskTagName() {
+		return KEY_JTRAC_TASK;
+	}
+	
+	@Override
+	public String getQueryHitTagName() {
+		return KEY_JTRAC_QUERY_HIT;
+	}	
+	
+	@Override
+	public String getQueryTagNameForElement(AbstractRepositoryQuery query) {
+		if (query instanceof JtracRepositoryQuery) {
+			return KEY_JTRAC_QUERY;
+		}
+		return "";
+	}	
+	
 }
