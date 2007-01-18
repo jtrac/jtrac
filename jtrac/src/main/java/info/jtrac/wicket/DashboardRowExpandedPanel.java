11deedab78a5c569c5272a0298a9e048cfe3f82a
Peter Thomas <ptrthomas@gmail.com>
2007-01-18 14:56:01 +0000

    hibernate open session in view applied for wicket servlet

--- jtrac/src/main/java/info/jtrac/wicket/DashboardRowExpandedPanel.java
+++ jtrac/src/main/java/info/jtrac/wicket/DashboardRowExpandedPanel.java
@@ -2,9 +2,11 @@ package info.jtrac.wicket;
 
 import info.jtrac.domain.Counts;
 import info.jtrac.domain.Space;
+import info.jtrac.domain.State;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import wicket.behavior.SimpleAttributeModifier;
 import wicket.markup.html.basic.Label;
 import wicket.markup.html.list.ListItem;
@@ -14,32 +16,36 @@ import wicket.model.PropertyModel;
 public class DashboardRowExpandedPanel extends BasePanel {    
     
     public DashboardRowExpandedPanel(String id, final Counts counts, Space space) {
+        
         super(id);
         setOutputMarkupId(true);
         
         SimpleAttributeModifier sam = new SimpleAttributeModifier("rowspan", counts.getTotalMap().size() + "");
+        
+        Map<Integer, String> states = new TreeMap(space.getMetadata().getStates());
+        states.remove(State.NEW);        
+        List<Integer> stateKeys = new ArrayList<Integer>(states.keySet());
+        
+        int first = stateKeys.get(0);
         
         add(new Label("space", "SPACE").add(sam));
         add(new Label("new", "NEW").add(sam));
         add(new Label("search", "SEARCH").add(sam));
         add(new Label("pink", "(-)").add(sam));                
         add(new Label("status", "XXX"));
-        add(new Label("loggedByMe", new PropertyModel(counts, "loggedByMeMap[1]")));
-        add(new Label("assignedToMe", new PropertyModel(counts, "assignedToMeMap[1]")));
-        add(new Label("total", new PropertyModel(counts, "totalMap[1]")));                
+        add(new Label("loggedByMe", counts.getLoggedByMeMap().get(first) + ""));
+        add(new Label("assignedToMe", counts.getAssignedToMeMap().get(first) + ""));
+        add(new Label("total", counts.getTotalMap().get(first) + ""));                      
         
-        // Map<Integer, String> states = space.getMetadata().getStates();
-
-        List<Integer> stateKeys = new ArrayList<Integer>();
-        stateKeys.add(1);
-        stateKeys.add(99);
+        stateKeys.remove(0);
+        
         add(new ListView("rows", stateKeys) {
             protected void populateItem(ListItem listItem) {
                 Integer i = (Integer) listItem.getModelObject();
                 listItem.add(new Label("status", listItem.getIndex() + ""));
-                listItem.add(new Label("loggedByMe", new PropertyModel(counts, "loggedByMeMap[" + listItem.getIndex() + "]")));
-                listItem.add(new Label("assignedToMe", new PropertyModel(counts, "assignedToMeMap[" + listItem.getIndex() + "]")));
-                listItem.add(new Label("total", new PropertyModel(counts, "totalMap[" + listItem.getIndex() + "]")));                 
+                listItem.add(new Label("loggedByMe", counts.getLoggedByMeMap().get(i) + ""));
+                listItem.add(new Label("assignedToMe", counts.getAssignedToMeMap().get(i) + ""));
+                listItem.add(new Label("total", counts.getTotalMap().get(i) + ""));                
             }
             
         });
