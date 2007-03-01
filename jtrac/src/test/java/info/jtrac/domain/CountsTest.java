77b5ecf659f5fbe75956819258709fc9917467a5
Peter Thomas <ptrthomas@gmail.com>
2007-03-01 05:55:34 +0000

    fixed unit test for counts

--- jtrac/src/test/java/info/jtrac/domain/CountsTest.java
+++ jtrac/src/test/java/info/jtrac/domain/CountsTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestCase;
 public class CountsTest extends TestCase {
     
     public void testCountsLogic() {
-        Counts c = new Counts();
+        Counts c = new Counts(false);
         c.add(Counts.ASSIGNED_TO_ME, 1, 5);        
         assertEquals(0, c.getTotal());
         assertEquals(5, c.getAssignedToMe());
