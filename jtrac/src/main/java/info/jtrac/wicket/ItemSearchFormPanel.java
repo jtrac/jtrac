7d606a7ee0187efb1a8e38818f9bc7fd3aee88b0
Peter Thomas <ptrthomas@gmail.com>
2007-03-10 12:13:27 +0000

    some grunt work to get the selected highlighting on search form working

--- jtrac/src/main/java/info/jtrac/wicket/ItemSearchFormPanel.java
+++ jtrac/src/main/java/info/jtrac/wicket/ItemSearchFormPanel.java
@@ -23,9 +23,12 @@ import info.jtrac.domain.User;
 import info.jtrac.util.UserUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import wicket.Component;
+import wicket.behavior.AbstractBehavior;
+import wicket.markup.ComponentTag;
 import wicket.markup.html.WebMarkupContainer;
 import wicket.markup.html.basic.Label;
 import wicket.markup.html.form.CheckBox;
@@ -42,6 +45,7 @@ import wicket.markup.html.list.ListView;
 import wicket.markup.html.panel.FeedbackPanel;
 import wicket.model.AbstractReadOnlyModel;
 import wicket.model.BoundCompoundPropertyModel;
+import wicket.model.PropertyModel;
 
 /**
  * item search form panel
@@ -67,6 +71,28 @@ public class ItemSearchFormPanel extends BasePanel {
         itemSearch.setCurrentPage(0);        
         add(new ItemSearchForm("form", itemSearch));      
     }    
+    
+    private class SelectedHighlighter extends AbstractBehavior {
+    
+        FormComponent fc;        
+        
+        public SelectedHighlighter(FormComponent fc) {            
+            this.fc = fc;
+        }        
+        
+        @Override
+        public void onComponentTag(Component c, ComponentTag tag) {
+            Object o = fc.getModelObject();
+            if(o == null) {
+                return;
+            }
+            if(o instanceof Collection && ((Collection) o).size() == 0) {
+                return;
+            }
+            tag.put("class", "selected");
+        }
+        
+    }
 
     private class ItemSearchForm extends Form {
         
@@ -89,7 +115,11 @@ public class ItemSearchFormPanel extends BasePanel {
             super(id);
             this.itemSearch = itemSearch;
             addComponents();
-        }                    
+        }                            
+        
+        private Component getSelecter(FormComponent fc) {
+            return new WebMarkupContainer(fc.getId() + "Label").add(new SelectedHighlighter(fc));
+        }                 
         
         private void addComponents() {
             final BoundCompoundPropertyModel model = new BoundCompoundPropertyModel(itemSearch);
@@ -131,6 +161,7 @@ public class ItemSearchFormPanel extends BasePanel {
                 } 
             });
             summary.add(new ErrorHighlighter());
+            add(getSelecter(summary));
             add(summary);
             // page size =======================================================
             List<Integer> sizes = Arrays.asList(new Integer[] { 5, 10, 15, 25, 50, 100, -1 });
@@ -144,10 +175,34 @@ public class ItemSearchFormPanel extends BasePanel {
                 }
             });
             add(pageSizeChoice);
+            add(new WebMarkupContainer("pageSizeLabel").add(new AbstractBehavior() {
+                @Override
+                public void onComponentTag(Component c, ComponentTag tag) {
+                    if(itemSearch.getPageSize() != 25) {
+                        tag.put("class", "selected");
+                    }
+                }                
+            }));
             // show detail =====================================================
             add(new CheckBox("showDetail"));
+            add(new WebMarkupContainer("showDetailLabel").add(new AbstractBehavior() {
+                @Override
+                public void onComponentTag(Component c, ComponentTag tag) {
+                    if(itemSearch.isShowDetail()) {
+                        tag.put("class", "selected");
+                    }
+                }                
+            }));                      
             // show history ====================================================
             add(new CheckBox("showHistory"));
+            add(new WebMarkupContainer("showHistoryLabel").add(new AbstractBehavior() {
+                @Override
+                public void onComponentTag(Component c, ComponentTag tag) {
+                    if(itemSearch.isShowHistory()) {
+                        tag.put("class", "selected");
+                    }
+                }                
+            }));             
             // severity / priority =============================================
             WebMarkupContainer sp = new WebMarkupContainer("severityPriority");
             if (itemSearch.getSpace() == null) {                
@@ -165,6 +220,7 @@ public class ItemSearchFormPanel extends BasePanel {
                     }                
                 });            
                 sp.add(severityListChoice);
+                sp.add(getSelecter(severityListChoice));
                 final Map<String, String> priorityMap = itemSearch.getPriorityOptions();
                 List<Integer> priorityList = new ArrayList(priorityMap.size());
                 for(String s : priorityMap.keySet()) {
@@ -179,6 +235,7 @@ public class ItemSearchFormPanel extends BasePanel {
                     }                
                 });            
                 sp.add(priorityListChoice);
+                sp.add(getSelecter(priorityListChoice));
             } else {
                 sp.setVisible(false);
             }
@@ -195,6 +252,7 @@ public class ItemSearchFormPanel extends BasePanel {
                 }                
             });            
             add(statusListChoice);
+            add(getSelecter(statusListChoice));
             // =================================================================
             List<User> users = null;
             if (itemSearch.getSpace() == null) {
@@ -214,16 +272,51 @@ public class ItemSearchFormPanel extends BasePanel {
             // loggedBy ========================================================
             ListMultipleChoice loggedByChoice = new ListMultipleChoice("loggedByList", users, userChoiceRenderer);
             add(loggedByChoice);
+            add(getSelecter(loggedByChoice));
             // assignedTo ======================================================
             ListMultipleChoice assignedToChoice = new ListMultipleChoice("assignedToList", users, userChoiceRenderer);            
             add(assignedToChoice);
+            add(getSelecter(assignedToChoice));
             // dates ===========================================================
             String createdDateLabel = getLocalizer().getString("item_search_form.createdDate", null);
-            String modifiedDateLabel = getLocalizer().getString("item_search_form.historyUpdatedDate", null);
+            String modifiedDateLabel = getLocalizer().getString("item_search_form.historyUpdatedDate", null);            
             add(new DatePicker("createdDateStart", model, "createdDateStart", false, createdDateLabel));
+            // TODO refactor this better when date picker component improves
+            add(new WebMarkupContainer("createdDateStartLabel").add(new AbstractBehavior() {
+                @Override
+                public void onComponentTag(Component c, ComponentTag tag) {
+                    if(itemSearch.getCreatedDateStart() != null) {
+                        tag.put("class", "selected");
+                    }
+                }                
+            }));
             add(new DatePicker("createdDateEnd", model, "createdDateEnd", false, createdDateLabel));
+            add(new WebMarkupContainer("createdDateEndLabel").add(new AbstractBehavior() {
+                @Override
+                public void onComponentTag(Component c, ComponentTag tag) {
+                    if(itemSearch.getCreatedDateEnd() != null) {
+                        tag.put("class", "selected");
+                    }
+                }                
+            }));
             add(new DatePicker("modifiedDateStart", model, "modifiedDateStart", false, modifiedDateLabel));
+            add(new WebMarkupContainer("modifiedDateStartLabel").add(new AbstractBehavior() {
+                @Override
+                public void onComponentTag(Component c, ComponentTag tag) {
+                    if(itemSearch.getModifiedDateStart() != null) {
+                        tag.put("class", "selected");
+                    }
+                }                
+            }));
             add(new DatePicker("modifiedDateEnd", model, "modifiedDateEnd", false, modifiedDateLabel));
+            add(new WebMarkupContainer("modifiedDateEndLabel").add(new AbstractBehavior() {
+                @Override
+                public void onComponentTag(Component c, ComponentTag tag) {
+                    if(itemSearch.getModifiedDateEnd() != null) {
+                        tag.put("class", "selected");
+                    }
+                }                
+            }));
             // spaces ===========================================================
             WebMarkupContainer spaces = new WebMarkupContainer("spaces");
             if (itemSearch.getSpace() == null) {
@@ -238,6 +331,7 @@ public class ItemSearchFormPanel extends BasePanel {
                     }
                 });
                 spaces.add(spaceChoice);
+                spaces.add(getSelecter(spaceChoice));
             } else {
                 spaces.setVisible(false);
             }
@@ -247,14 +341,13 @@ public class ItemSearchFormPanel extends BasePanel {
                 ListView listView = new ListView("customDropDowns", itemSearch.getDropDownFields()) {
                     protected void populateItem(ListItem listItem) {
                         Field field = (Field) listItem.getModelObject();
-                        listItem.add(new Label("label", field.getLabel()));
                         final Map<String, String> options = field.getOptions();
                         List<Integer> optionKeys = new ArrayList<Integer>(options.size());
                         // the types have to match perfectly for binding TODO - remove JSP-ishness
                         for(String s : options.keySet()) {
                             optionKeys.add(new Integer(s));
                         }
-                        ListMultipleChoice spaceChoice = new ListMultipleChoice("field", optionKeys, new IChoiceRenderer() {
+                        final ListMultipleChoice fieldChoice = new ListMultipleChoice("field", optionKeys, new IChoiceRenderer() {
                             public Object getDisplayValue(Object o) {
                                 return options.get(o.toString());
                             }
@@ -262,7 +355,18 @@ public class ItemSearchFormPanel extends BasePanel {
                                 return o.toString();
                             }
                         });
-                        listItem.add(model.bind(spaceChoice, field.getName().getText() + "List"));
+                        listItem.add(model.bind(fieldChoice, field.getName().getText() + "List"));
+                        Label label = new Label("label", field.getLabel());
+                        label.add(new AbstractBehavior() {
+                            @Override
+                            public void onComponentTag(Component c, ComponentTag tag) {
+                                Object o = fieldChoice.getModelObject();
+                                if(o != null && ((Collection) o).size() > 0) {
+                                    tag.put("class", "selected");
+                                }
+                            }                
+                        });
+                        listItem.add(label);                        
                     }                    
                 };
                 add(listView);
@@ -275,8 +379,28 @@ public class ItemSearchFormPanel extends BasePanel {
             if (itemSearch.getSpace() != null) {
                 ListView listView = new ListView("customDates", itemSearch.getDateFields()) {
                     protected void populateItem(ListItem listItem) {
-                        Field field = (Field) listItem.getModelObject();
-                        listItem.add(new Label("label", field.getLabel()));
+                        final Field field = (Field) listItem.getModelObject();
+                        Label label = new Label("label", field.getLabel());
+                        listItem.add(label);
+                        // TODO improve and specific to on/after or on/before
+                        listItem.add(new WebMarkupContainer("startLabel").add(new AbstractBehavior() {
+                            @Override
+                            public void onComponentTag(Component c, ComponentTag tag) {
+                                PropertyModel modelStart = new PropertyModel(itemSearch, field.getName().getText() + "Start");                                
+                                if(modelStart.getObject(null) != null) {
+                                    tag.put("class", "selected");
+                                }
+                            }                
+                        }));
+                        listItem.add(new WebMarkupContainer("endLabel").add(new AbstractBehavior() {
+                            @Override
+                            public void onComponentTag(Component c, ComponentTag tag) {
+                                PropertyModel modelEnd = new PropertyModel(itemSearch, field.getName().getText() + "End");                                
+                                if(modelEnd.getObject(null) != null) {                                    
+                                    tag.put("class", "selected");
+                                }
+                            }                
+                        }));                        
                         listItem.add(new DatePicker("fieldStart", model, field.getName().getText() + "Start", false, field.getLabel()));
                         listItem.add(new DatePicker("fieldEnd", model, field.getName().getText() + "End", false, field.getLabel()));
                     }                    
@@ -293,9 +417,18 @@ public class ItemSearchFormPanel extends BasePanel {
                 ListView listView = new ListView("customTexts", itemSearch.getTextFields()) {
                     protected void populateItem(ListItem listItem) {
                         Field field = (Field) listItem.getModelObject();
-                        listItem.add(new Label("label", field.getLabel()));
-                        TextField textField = new TextField("field");
+                        Label label = new Label("label", field.getLabel());
+                        listItem.add(label);
+                        final TextField textField = new TextField("field");
                         listItem.add(model.bind(textField, field.getName().getText()));
+                        label.add(new AbstractBehavior() {
+                            @Override
+                            public void onComponentTag(Component c, ComponentTag tag) {
+                                if(textField.getModelObject() != null) {
+                                    tag.put("class", "selected");
+                                }
+                            }                
+                        });
                     }                    
                 };
                 listView.setReuseItems(true);
