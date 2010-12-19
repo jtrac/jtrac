b6e24ab66d904159c8eb50a12fb6ebd647ced59e
magog96 <magog96@unknown.email>
2010-12-19 20:23:37 +0000

    Added complete JavaDoc documentation to the DataSourceFactoryBean and SchemaHelper classes.

--- jtrac/src/main/java/info/jtrac/config/DataSourceFactoryBean.java
+++ jtrac/src/main/java/info/jtrac/config/DataSourceFactoryBean.java
@@ -29,61 +29,142 @@ import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.util.StringUtils;
 
 /**
- * spring factory bean to conditionally create the right data source
- * either embedded or apache dbcp or JNDI datasource
+ * <p>
+ * This class implements the Spring frameworks
+ * {@link org.springframework.beans.factory.FactoryBean} as well as the 
+ * {@link org.springframework.beans.factory.DisposableBean} to conditionally
+ * create the correct <code>DataSource</code> object to access the database.
+ * </p>
+ * 
+ * Supported data sources are:
+ * <ul>
+ *    <li><a href="http://hsqldb.org/">HSQLDB (embedded)</a></li>
+ *    <li><a href="http://commons.apache.org/dbcp/">Apache DBCP</a></li>
+ *    <li>Java Naming and Directory Interface (JNDI); supported by most JDBC
+ *    database drivers</li>
+ * </ul>
  */
 public class DataSourceFactoryBean implements FactoryBean, DisposableBean {
-    
+    /**
+     * Logger object
+     */
     private final Logger logger = LoggerFactory.getLogger(getClass());
     
+    /**
+     * DB driver class name
+     */
     private String driverClassName;
+    
+    /**
+     * DB url
+     */
     private String url;
+    
+    /**
+     * DB user
+     */
     private String username;
+    
+    /**
+     * DB password
+     */
     private String password;
+    
+    /**
+     * DB queries used to check if the database can be accessed
+     */
     private String validationQuery;
+    
+    /**
+     * DB JNDI data source name
+     */
     private String dataSourceJndiName;
     
-    private DataSource dataSource;        
-
+    /**
+     * DB JNDI data source object
+     */
+    private DataSource dataSource;
+    
+    /**
+     * This method allows to store the name of the DB driver class.
+     * 
+     * @param driverClassName The name of the DB driver class.
+     */
     public void setDriverClassName(String driverClassName) {
         this.driverClassName = driverClassName;
     }
-
+    
+    /**
+     * This method allows to store the DB url.
+     * 
+     * @param url The DB url.
+     */
     public void setUrl(String url) {
         this.url = url;
     }
-
+    
+    /**
+     * This method allows to store the DB user.
+     * 
+     * @param username The DB user.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
+    /**
+     * This method allows to store the DB password.
+     * 
+     * @param password The DB password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
+    /**
+     * This method allows to store the SQL query string used to check if
+     * the DB connection is working.
+     * 
+     * @param validationQuery The SQL query used to validate if the DB
+     * connection is working.
+     */
     public void setValidationQuery(String validationQuery) {
         this.validationQuery = validationQuery;
     }
-
+    
+    /**
+     * This method allows to store the JNDI data source name.
+     * 
+     * @param dataSourceJndiName The JNDI data source name.
+     */
     public void setDataSourceJndiName(String dataSourceJndiName) {
         this.dataSourceJndiName = dataSourceJndiName;
-    }        
+    }
     
+    /**
+     * This method returns the dataSource object used for the DB access.
+     * 
+     * @throws Exception
+     * @see org.springframework.beans.factory.FactoryBean#getObject()
+     */
     public Object getObject() throws Exception {
         if(StringUtils.hasText(dataSourceJndiName)) {
             logger.info("JNDI datasource requested, looking up datasource from JNDI name: '" + dataSourceJndiName + "'");
             JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
             factoryBean.setJndiName(dataSourceJndiName);
+            
             // "java:comp/env/" will be prefixed if the JNDI name doesn't already have it
             factoryBean.setResourceRef(true);
-            // this step actually does the JNDI lookup
+            
+            // This step actually does the JNDI lookup
             try {
                 factoryBean.afterPropertiesSet();
             } catch(Exception e) {
                 logger.error("datasource init from JNDI failed : " + e);
                 logger.error("aborting application startup");
                 throw new RuntimeException(e);
-            }                 
+            } // end try..catch
+            
             dataSource = (DataSource) factoryBean.getObject();
         } else if(url.startsWith("jdbc:hsqldb:file")) {
             logger.info("embedded HSQLDB mode detected, switching on spring single connection data source");
@@ -93,7 +174,7 @@ public class DataSourceFactoryBean implements FactoryBean, DisposableBean {
             ds.setUsername(username);
             ds.setPassword(password);
             ds.setSuppressClose(true);
-            dataSource = ds;            
+            dataSource = ds;
         } else {
             logger.info("Not using embedded HSQLDB or JNDI datasource, switching on Apache DBCP data source connection pooling");
             BasicDataSource ds = new BasicDataSource();
@@ -104,20 +185,42 @@ public class DataSourceFactoryBean implements FactoryBean, DisposableBean {
             ds.setValidationQuery(validationQuery);
             ds.setTestOnBorrow(false);
             ds.setTestWhileIdle(true);
-            ds.setTimeBetweenEvictionRunsMillis(600000);            
+            ds.setTimeBetweenEvictionRunsMillis(600000);
             dataSource = ds;
-        }
+        } // end if..else
+        
         return dataSource;
     }
-
+    
+    /**
+     * This method returns the class name of the DataSource object which can be
+     * used to determine which data source implementation is currently used.
+     *  
+     * @return Returns the class name <code>Object.class</code> of the
+     * DataSource object.
+     * @see org.springframework.beans.factory.FactoryBean#getObjectType()
+     */
     public Class getObjectType() {
         return DataSource.class;
     }
-
+    
+    /**
+     * This method returns if the factory is implemented as Singleton.
+     * 
+     * @return Returns if the factory is implemented as Singleton or not.
+     * @see org.springframework.beans.factory.FactoryBean#isSingleton()
+     */
     public boolean isSingleton() {
         return true;
-    }        
-
+    }
+    
+    /**
+     * This method is called to clean up the references when the object is
+     * destroyed.
+     * 
+     * @throws Exception
+     * @see org.springframework.beans.factory.DisposableBean#destroy()
+     */
     public void destroy() throws Exception {
         if(dataSource instanceof SingleConnectionDataSource) {
             logger.info("attempting to shut down embedded HSQLDB database");
@@ -133,7 +236,6 @@ public class DataSourceFactoryBean implements FactoryBean, DisposableBean {
             logger.info("Apache DBCP data source closed successfully");
         } else {
             logger.info("context shutting down for JNDI datasource");
-        }
+        } // end if..else
     }
-    
 }
--- jtrac/src/main/java/info/jtrac/hibernate/SchemaHelper.java
+++ jtrac/src/main/java/info/jtrac/hibernate/SchemaHelper.java
@@ -23,70 +23,144 @@ import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
 /**
- * Utilities to create the database schema, drop and create tables
- * Uses Hibernate Schema tools
- * Used normally at application first start
+ * <p>
+ * Utilities to create the database schema, drop and create tables by using
+ * the Hibernate schema tools.
+ * </p>
+ * <p>
+ * This method is normally called at the first start of the application to
+ * initialize the database.
+ * </p>
  */
 public class SchemaHelper {
-    
+    /**
+     * Logger object
+     */
     private final Logger logger = LoggerFactory.getLogger(getClass());
     
+    /**
+     * DB driver class name
+     */
     private String driverClassName;
+    
+    /**
+     * DB url
+     */
     private String url;
+    
+    /**
+     * DB user
+     */
     private String username;
+    
+    /**
+     * DB password
+     */
     private String password;
+    
+    /**
+     * DB hibernate dialect
+     */
     private String hibernateDialect;
+    
+    /**
+     * DB JNDI data source name
+     */
     private String dataSourceJndiName;
-    private String[] mappingResources;    
-
+    
+    /**
+     * Mapping information
+     */
+    private String[] mappingResources;
+    
+    /**
+     * This method allows to store the name of the DB driver class.
+     * 
+     * @param driverClassName The name of the DB driver class.
+     */
     public void setDriverClassName(String driverClassName) {
         this.driverClassName = driverClassName;
     }
-
+    
+    /**
+     * This method allows to store the Hibernate dialect required for the
+     * specified database. You can find the list of supported dialects in the
+     * Hibernate documentation.
+     * 
+     * @param hibernateDialect The dialect name supported by Hibernate.
+     */
     public void setHibernateDialect(String hibernateDialect) {
         this.hibernateDialect = hibernateDialect;
     }
     
+    /**
+     * This method allows to store multiple mapping resources.
+     * 
+     * @param mappingResources The String array of mapping Resources.
+     */
     public void setMappingResources(String[] mappingResources) {
         this.mappingResources = mappingResources;
-    }    
+    }
     
+    /**
+     * This method allows to store the DB url.
+     * 
+     * @param url The DB url.
+     */
     public void setUrl(String url) {
         this.url = url;
     }
-
+    
+    /**
+     * This method allows to store the DB user.
+     * 
+     * @param username The DB user.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
+    /**
+     * This method allows to store the DB password.
+     * 
+     * @param password The DB password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public void setDataSourceJndiName(String dataSourceJndiName) {
-        this.dataSourceJndiName = dataSourceJndiName;
-    }        
     
     /**
-     * create tables using the given Hibernate configuration
+     * This method allows to store the JNDI data source name.
+     * 
+     * @param dataSourceJndiName The JNDI data source name.
+     */
+    public void setDataSourceJndiName(String dataSourceJndiName) {
+        this.dataSourceJndiName = dataSourceJndiName;
+    }
+    
+    /**
+     * Create tables using the given Hibernate configuration data.
      */
     public void createSchema() {
         Configuration cfg = new Configuration();
+        
         if(StringUtils.hasText(dataSourceJndiName)) {
             cfg.setProperty("hibernate.connection.datasource", dataSourceJndiName);
         } else {
-            cfg.setProperty("hibernate.connection.driver_class", driverClassName);        
+            cfg.setProperty("hibernate.connection.driver_class", driverClassName);
             cfg.setProperty("hibernate.connection.url", url);
             cfg.setProperty("hibernate.connection.username", username);
             cfg.setProperty("hibernate.connection.password", password);
-        }
-        cfg.setProperty("hibernate.dialect", hibernateDialect);        
+        } // end if..else
+        
+        cfg.setProperty("hibernate.dialect", hibernateDialect);
+        
         for (String resource : mappingResources) {
             cfg.addResource(resource);
-        }
+        } // end for
+        
         logger.info("begin database schema creation =========================");
         new SchemaUpdate(cfg).execute(true, true);
         logger.info("end database schema creation ===========================");
     }
-    
 }
