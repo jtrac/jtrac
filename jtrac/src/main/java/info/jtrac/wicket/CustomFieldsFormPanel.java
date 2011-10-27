ecd2abe0e2c01f4aaae84a4d7fcb6ce4a18eb092
magog96 <magog96@unknown.email>
2011-10-27 19:59:49 +0000

    -added JavaDoc comments -updated inline comments -reformatted source code as well as HTML code

--- jtrac/src/main/java/info/jtrac/wicket/CustomFieldsFormPanel.java
+++ jtrac/src/main/java/info/jtrac/wicket/CustomFieldsFormPanel.java
@@ -37,74 +37,113 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
 /**
- * panel for custom fields that can be reused in the ite-create / item-view forms
+ * This class is responsible for the panel of custom fields that
+ * can be reused in the item-create/item-view forms.
  */
-public class CustomFieldsFormPanel extends BasePanel {        
-    
+public class CustomFieldsFormPanel extends BasePanel {
+    /**
+     * Constructor
+     * 
+     * @param id
+     * @param model
+     * @param space
+     */
     public CustomFieldsFormPanel(String id, BoundCompoundPropertyModel model, Space space) {
         super(id);
         List<Field> fields = space.getMetadata().getFieldList();
         addComponents(model, fields, true);
-    }    
+    }
     
+    /**
+     * Constructor
+     * 
+     * @param id
+     * @param model
+     * @param item
+     * @param user
+     */
     public CustomFieldsFormPanel(String id, BoundCompoundPropertyModel model, Item item, User user) {
         super(id);
         List<Field> fields = item.getEditableFieldList(user);
         addComponents(model, fields, false);
     }
     
+    /**
+     * This method allows to add components (custom fields).
+     * 
+     * @param model
+     * @param fields
+     * @param isEditMode
+     */
     private void addComponents(final BoundCompoundPropertyModel model, List<Field> fields, final boolean isEditMode) {
         ListView listView = new ListView("fields", fields) {
-            protected void populateItem(ListItem listItem) {                
+            protected void populateItem(ListItem listItem) {
                 final Field field = (Field) listItem.getModelObject();
-                boolean isRequired = isEditMode ? false : !field.isOptional();             
+                boolean isRequired = isEditMode ? false : !field.isOptional();
                 listItem.add(new Label("label", field.getLabel()));
                 listItem.add(new Label("star", isRequired ? "*" : "&nbsp;").setEscapeModelStrings(false));
-                if (field.isDropDownType()) {                  
+                if (field.isDropDownType()) {
                     Fragment f = new Fragment("field", "dropDown", CustomFieldsFormPanel.this);
                     final Map<String, String> options = field.getOptions();
                     List<String> keys; // bound value
-                    if(options != null) {
-                        keys = new ArrayList(options.keySet());  
+                    if (options != null) {
+                        keys = new ArrayList(options.keySet());
                     } else {
                         keys = new ArrayList<String>();
-                    }                    
+                    }
                     DropDownChoice choice = new DropDownChoice("field", keys, new IChoiceRenderer() {
                         public Object getDisplayValue(Object o) {
                             return options.get(o);
                         };
+                        
                         public String getIdValue(Object o, int i) {
                             return o.toString();
                         };
                     });
-                    choice.setNullValid(true);                                      
-                    choice.setLabel(new Model(field.getLabel()));                                            
+                    choice.setNullValid(true);
+                    choice.setLabel(new Model(field.getLabel()));
                     choice.setRequired(isRequired);
                     WebMarkupContainer border = new WebMarkupContainer("border");
                     f.add(border);
                     border.add(new ErrorHighlighter(choice));
-                    border.add(model.bind(choice, field.getName().getText()));                    
+                    border.add(model.bind(choice, field.getName().getText()));
                     listItem.add(f);
-                } else if (field.isDatePickerType()) {                   
-                    YuiCalendar calendar = new YuiCalendar("field", new PropertyModel(model, field.getName().getText()), isRequired);
+                } else if (field.isDatePickerType()) {
+                    /*
+                     * ======================================
+                     * Date picker
+                     * ======================================
+                     */
+                    YuiCalendar calendar = new YuiCalendar("field",
+                            new PropertyModel(model, field.getName().getText()), isRequired);
                     listItem.add(calendar);
                     calendar.setLabel(new Model(field.getLabel()));
                 } else {
+                    /*
+                     * ======================================
+                     * Text field
+                     * ======================================
+                     */
                     Fragment f = new Fragment("field", "textField", CustomFieldsFormPanel.this);
                     TextField textField = new TextField("field");
+                    
+                    /*
+                     * Check if the field is used to display/edit
+                     * Double values.
+                     */
                     if (field.isDecimalNumberType()) {
                         textField.setType(Double.class);
                     }
-                    textField.add(new ErrorHighlighter());                    
-                    textField.setRequired(isRequired);                    
-                    textField.setLabel(new Model(field.getLabel()));                        
+                    
+                    textField.add(new ErrorHighlighter());
+                    textField.setRequired(isRequired);
+                    textField.setLabel(new Model(field.getLabel()));
                     f.add(model.bind(textField, field.getName().getText()));
                     listItem.add(f);
                 }
             }
         };
         listView.setReuseItems(true);
-        add(listView);        
-    }
-    
+        add(listView);
+    } // end method addComponents(...)
 }
--- jtrac/src/main/java/info/jtrac/wicket/ItemFormPage.java
+++ jtrac/src/main/java/info/jtrac/wicket/ItemFormPage.java
@@ -49,48 +49,65 @@ import org.apache.wicket.util.lang.Bytes;
 /**
  * Create / Edit item form page
  */
-public class ItemFormPage extends BasePage {    
-            
-    public ItemFormPage() {        
+public class ItemFormPage extends BasePage {
+    /**
+     * Default constructor
+     */
+    public ItemFormPage() {
         Item item = new Item();
         item.setSpace(getCurrentSpace());
-        item.setStatus(State.NEW);        
+        item.setStatus(State.NEW);
         add(new ItemForm("form", item));
     }
     
-    public ItemFormPage(long itemId) {        
-        Item item = getJtrac().loadItem(itemId);      
+    /**
+     * Constructor with provided item id.
+     * 
+     * @param itemId The 
+     */
+    public ItemFormPage(long itemId) {
+        Item item = getJtrac().loadItem(itemId);
         add(new ItemForm("form", item));
-    }    
+    }
     
     /**
-     * wicket form
-     */    
+     * Wicket form
+     */
     private class ItemForm extends Form {
-        
         private JtracFeedbackMessageFilter filter;
         private FileUploadField fileUploadField = new FileUploadField("file");
         private boolean editMode;
-        private int version;        
+        private int version;
         
+        /**
+         * Constructor
+         * 
+         * @param id
+         * @param item
+         */
         public ItemForm(String id, final Item item) {
             super(id);
             setMultiPart(true);
             FeedbackPanel feedback = new FeedbackPanel("feedback");
             filter = new JtracFeedbackMessageFilter();
             feedback.setFilter(filter);
-            add(feedback);   
+            add(feedback);
             version = item.getVersion();
-            if(item.getId() > 0) {
-                editMode = true;                
+            
+            if (item.getId() > 0) {
+                editMode = true;
             }
+            
             BoundCompoundPropertyModel model = null;
-            if(editMode) {
-                // this ensures that the model object is re-loaded as part of the
-                // form submission workflow before form binding and avoids
-                // hibernate lazy loading issues during the whole update transaction
+            if (editMode) {
+                /*
+                 * This ensures that the model object is re-loaded as part
+                 * of the form submission workflow before form binding and
+                 * avoids hibernate lazy loading issues during the whole
+                 * update transaction.
+                 */
                 LoadableDetachableModel itemModel = new LoadableDetachableModel() {
-                    protected Object load() {                                        
+                    protected Object load() {
                         logger.debug("attaching existing item " + item.getId());
                         return getJtrac().loadItem(item.getId());
                     }
@@ -99,8 +116,13 @@ public class ItemFormPage extends BasePage {
             } else {
                 model = new BoundCompoundPropertyModel(item);
             }
-            setModel(model);                        
-            // summary =========================================================
+            setModel(model);
+            
+            /*
+             * ===================================================
+             * Summary
+             * ===================================================
+             */
             final TextField summaryField = new TextField("summary");
             summaryField.setRequired(true);
             summaryField.add(new ErrorHighlighter());
@@ -108,58 +130,83 @@ public class ItemFormPage extends BasePage {
             add(summaryField);
             add(new HeaderContributor(new IHeaderContributor() {
                 public void renderHead(IHeaderResponse response) {
-                    response.renderOnLoadJavascript("document.getElementById('" + summaryField.getMarkupId() + "').focus()");
+                    response.renderOnLoadJavascript("document.getElementById('" + 
+                            summaryField.getMarkupId() + "').focus()");
                 }
             }));
-            // delete button ===================================================
+            
+            /*
+             * ===================================================
+             * Delete button
+             * ===================================================
+             */
             Button delete = new Button("delete") {
                 @Override
                 public void onSubmit() {
                     String heading = localize("item_delete.confirm");
                     String warning = localize("item_delete.line2");
-                    String line1 = localize("item_delete.line1");                    
-                    ConfirmPage confirm = new ConfirmPage(ItemFormPage.this, heading, warning, new String[] {line1}) {
+                    String line1 = localize("item_delete.line1");
+                    ConfirmPage confirm = new ConfirmPage(ItemFormPage.this, heading, warning, new String[] { line1 }) {
                         public void onConfirm() {
                             // avoid lazy init problem
                             getJtrac().removeItem(getJtrac().loadItem(item.getId()));
                             ItemSearch itemSearch = JtracSession.get().getItemSearch();
-                            if(itemSearch != null) {
+                            if (itemSearch != null) {
                                 setResponsePage(new ItemListPage(itemSearch));
                             } else {
                                 setResponsePage(DashboardPage.class);
                             }
-                        }                        
+                        }
                     };
-                    setResponsePage(confirm);                    
+                    setResponsePage(confirm);
                 }
             };
+            
             delete.setDefaultFormProcessing(false);
             add(delete);
-            if(!editMode) {
+            if (!editMode) {
                 delete.setVisible(false);
             }
-            // detail ==========================================================
+            
+            /*
+             * ===================================================
+             * Detail
+             * ===================================================
+             */
             add(new TextArea("detail").setRequired(true).add(new ErrorHighlighter()));
-            // custom fields ===================================================
-            if(editMode) {
+            
+            /*
+             * ===================================================
+             * Custom fields
+             * ===================================================
+             */
+            if (editMode) {
                 add(new CustomFieldsFormPanel("fields", model, item.getSpace()).setRenderBodyOnly(true));
             } else {
                 add(new CustomFieldsFormPanel("fields", model, item, getPrincipal()).setRenderBodyOnly(true));
             }
-            // hide some components if editing item
+            
+            /*
+             * Hide some components if editing item
+             */
             WebMarkupContainer hideAssignedTo = new WebMarkupContainer("hideAssignedTo");
             WebMarkupContainer hideNotifyList = new WebMarkupContainer("hideNotifyList");
             WebMarkupContainer hideEditReason = new WebMarkupContainer("hideEditReason");
             add(hideAssignedTo);
             add(hideNotifyList);
             add(hideEditReason);
-            if(editMode) {
+            if (editMode) {
                 hideAssignedTo.setVisible(false);
                 hideNotifyList.setVisible(false);
                 hideEditReason.add(new TextArea("editReason").setRequired(true).add(new ErrorHighlighter()));
             } else {
                 hideEditReason.setVisible(false);
-                // assigned to =================================================
+                
+                /*
+                 * ===================================================
+                 * Assigned to
+                 * ===================================================
+                 */
                 Space space = item.getSpace();
                 List<UserSpaceRole> userSpaceRoles = getJtrac().findUserRolesForSpace(space.getId());
                 List<User> assignable = UserUtils.filterUsersAbleToTransitionFrom(userSpaceRoles, space, State.OPEN);
@@ -167,69 +214,104 @@ public class ItemFormPage extends BasePage {
                     public Object getDisplayValue(Object o) {
                         return ((User) o).getName();
                     }
+                    
                     public String getIdValue(Object o, int i) {
                         return ((User) o).getId() + "";
-                    }                
+                    }
                 });
+                
                 choice.setNullValid(true);
                 choice.setRequired(true);
                 WebMarkupContainer border = new WebMarkupContainer("border");
                 border.add(choice);
                 border.add(new ErrorHighlighter(choice));
                 hideAssignedTo.add(border);
-                // notify list =================================================
+                
+                /*
+                 * ===================================================
+                 * Notify list
+                 * ===================================================
+                 */
                 List<ItemUser> choices = UserUtils.convertToItemUserList(userSpaceRoles);
-                ListMultipleChoice itemUsers = new JtracCheckBoxMultipleChoice("itemUsers", choices, new IChoiceRenderer() {
-                    public Object getDisplayValue(Object o) {
-                        return ((ItemUser) o).getUser().getName();
-                    }
-                    public String getIdValue(Object o, int i) {
-                        return ((ItemUser) o).getUser().getId() + "";
-                    }               
-                }, true);
+                ListMultipleChoice itemUsers = new JtracCheckBoxMultipleChoice("itemUsers", choices,
+                        new IChoiceRenderer() {
+                            public Object getDisplayValue(Object o) {
+                                return ((ItemUser) o).getUser().getName();
+                            }
+                            
+                            public String getIdValue(Object o, int i) {
+                                return ((ItemUser) o).getUser().getId() + "";
+                            }
+                        }, true);
                 hideNotifyList.add(itemUsers);
-                // attachment ==================================================                                                
+                
+                /*
+                 * ===================================================
+                 * Attachment
+                 * ===================================================
+                 */
                 hideNotifyList.add(fileUploadField);
                 setMaxSize(Bytes.megabytes(getJtrac().getAttachmentMaxSizeInMb()));
             }
-            // send notifications ==========================================
+            
+            /*
+             * ===================================================
+             * Send notifications
+             * ===================================================
+             */
             add(new CheckBox("sendNotifications"));
-            // cancel ==========================================================
+            
+            /*
+             * ===================================================
+             * Cancel
+             * ===================================================
+             */
             add(new Link("cancel") {
                 public void onClick() {
                     setResponsePage(ItemViewPage.class, new PageParameters("0=" + item.getRefId()));
-                }                
-            }.setVisible(editMode && JtracSession.get().getItemSearch() != null));            
+                }
+            }.setVisible(editMode && JtracSession.get().getItemSearch() != null));
         }
         
+        /* (non-Javadoc)
+         * @see org.apache.wicket.markup.html.form.Form#validate()
+         */
         @Override
         protected void validate() {
-            filter.reset();     
-            Item item = (Item) getModelObject();                      
-            if(editMode && item.getVersion() != version) {                                
-                // user must have used back button after edit
-                error(localize("item_form.error.version"));                                                    
-            } 
-            super.validate();    
-        }        
+            filter.reset();
+            Item item = (Item) getModelObject();
+            if (editMode && item.getVersion() != version) {
+                /*
+                 * User must have used back button after edit
+                 */
+                error(localize("item_form.error.version"));
+            }
+            super.validate();
+        }
         
+        /* (non-Javadoc)
+         * @see org.apache.wicket.markup.html.form.Form#onSubmit()
+         */
         @Override
         protected void onSubmit() {
             final FileUpload fileUpload = fileUploadField.getFileUpload();
-            Item item = (Item) getModelObject();                                                
-            User user = getPrincipal();            
-            if(editMode) {
-                getJtrac().updateItem(item, user);                
+            Item item = (Item) getModelObject();
+            User user = getPrincipal();
+            if (editMode) {
+                getJtrac().updateItem(item, user);
             } else {
                 item.setLoggedBy(user);
-                item.setStatus(State.OPEN);                
-                getJtrac().storeItem(item, fileUpload);                
-            }                   
-            // on creating an item, clear any search filter (especially the related item) from session
+                item.setStatus(State.OPEN);
+                getJtrac().storeItem(item, fileUpload);
+            }
+            
+            /*
+             * On creating an item, clear any search filter (especially
+             * the related item) from session.
+             */
             JtracSession.get().setItemSearch(null);
             setResponsePage(ItemViewPage.class, new PageParameters("0=" + item.getRefId()));
         }
         
-    }
-    
+    } // end inner class ItemForm
 }
